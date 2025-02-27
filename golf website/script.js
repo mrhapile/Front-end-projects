d767c12d68922dcd9825fc6c67b62c0c4997ea7c
mrhapile <allinonegaming3456@gmail.com>
2025-02-27 06:53:15 +0530

    scripting GSAP

---
+++ golf website/script.js
@@ -0,0 +1,26 @@
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 1.2,
+    height: "90px",
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+    },
+});
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "#body",
+        markers:true,
+        // start:"top -50%",
+
+    }
+})
+
+  
